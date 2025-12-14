bf15a8a7748be84926ec232db0713c136ff6d29c
Ajitha <ajitha.wipro@gmail.com>
2025-12-14 17:19:18 +0530

    react commit

---
+++ src/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import react from "react"
+
+function FirstComponent(){
+    return (
+     <div>
+        <h2>Welcome! This my first React Component</h2>
+     </div>
+    )
+}
+
+export default FirstComponent
